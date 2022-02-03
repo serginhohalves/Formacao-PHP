c700a52f3bbed1d2382ea02a334f2af602ad85df
serginhohalves <serginho.halves@hotmail.com>
2022-02-02 22:51:23 -0300

    secont commit

---
+++ heranca/exemplo.php
@@ -0,0 +1,33 @@
+<?php
+   
+   class Documento
+   {
+       private $numero;
+       public function getNumero()
+       {
+           return $this->numero;
+       }
+       public function setNumero($n)
+       {
+           $this->numero = $n;
+       }
+       
+   }
+
+   class CPF extends Documento
+   {
+       public function validar():bool
+       {
+            return true;
+       }
+
+   }
+    
+
+   $doc = new CPF();
+   $doc->setNumero("012012012-12");
+   var_dump($doc->validar());
+   echo "<br/>";
+   echo $doc->getNumero();
+  
+?>
