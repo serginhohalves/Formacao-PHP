b7929309c2274556c06ad273c2f7148e4d4b47ce
serginhohalves <serginho.halves@hotmail.com>
2022-02-06 10:12:04 -0300

    commit polimosfismo

---
+++ polimorfismo/exemplo..php
@@ -0,0 +1,65 @@
+<?php
+
+    abstract class Animal
+    {
+        public function falar()
+        {
+            return "Som";
+        }
+
+        public function mover()
+        {
+            return "Anda";
+        }
+
+    }   
+
+    class Cachorro extends Animal
+    {
+        public function falar()
+        {
+            return "Late";
+        }
+        public function mover()
+        {
+            return "Corre";
+        }
+    }
+
+    class Gato extends Animal
+    {
+        public function falar()
+        {
+            return "Mia";
+        }
+    }
+
+
+    class Passaro extends Animal{
+        public function falar()
+        {
+            return "Pia";
+        }
+
+        public function mover()
+        {
+            return "Voa e ". parent::mover();
+        }
+    }
+
+    $pluto = new Cachorro();
+    echo $pluto -> falar() . "<br>";
+    echo $pluto -> mover() .    "<br>";
+    echo "------------------------<br>  ";
+    $garfield = new Gato();
+    echo $garfield-> falar() . "<br>";
+    echo $garfield-> mover() . "<br>";
+    echo "------------------------<br>  ";
+    $piupiu = new Passaro();
+    echo $piupiu-> falar() . "<br>";
+    echo $piupiu-> mover() . "<br>";
+
+
+
+
+?>
