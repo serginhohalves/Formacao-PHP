b01863eb328cfffbb7e49cacb96e3efe518d2df0
serginhohalves <serginho.halves@hotmail.com>
2022-02-26 00:45:43 -0300

    banco de dados

---
+++ banco-de-dados/conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+$hostname = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "test";
+
+
+$conn = mysqli_connect($hostname, $dbUsername, $dbPassword, $dbName);
+// if($conn -> connect_error){
+//     die("Falha na conexão: " . $conn -> connect_error);
+// }else{
+//     echo "Conexão realizada com sucesso!";
+// }
+
+
+?>
